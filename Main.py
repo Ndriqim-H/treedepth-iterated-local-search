12c7a2297bd9e85b1a7521235efe93e8016939f4
Blend Arifaj <blendarifaj@hotmail.com>
2020-03-28 22:14:04 +0100

    update main

--- Main.py
+++ Main.py
@@ -5,8 +5,10 @@ from SignalHandler import signal_handler
 if __name__ == '__main__':
     signal_handler = signal_handler()
     tree = treedepth("public/heur_001.gr")
-    alg = hill_climbing(tree.matrix, tree.total_points)
-    while not signal_handler.finish_proccess:
-        alg.move()
-    alg.calculate_full_fitness(alg.solution)
-    alg.save_solution("heur_001_solution.out")
+    ##alg = hill_climbing(tree.matrix,tree.total_points)
+    tabu_search_algorithm = tabu_search(tree.matrix)
+    tabu_search_algorithm.ts_algorithm()
+    #while not signal_handler.finish_proccess:
+    #  alg.specific_move()
+    #alg.calculate_full_fitness(alg.solution)
+    #alg.save_solution("heur_001_solution.out")
